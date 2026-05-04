87aa27484efbfd63f60f1daa199d0bf8c3eb69f5
Hemanth Reddy <ragipindihemanth713@gmail.com>
2026-05-05 01:50:47 +0530

    Deployment: Add Procfile and simplify wsgi entry point

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
-import sys
 import os
+import sys
 
 # Add backend to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
-from app import create_app
-app = create_app('prod')
+from app import app as application
+app = application
 
 if __name__ == "__main__":
     app.run()
